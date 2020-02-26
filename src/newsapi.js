8b7594673b3600649868b8b660080a24862994b1
tanujasheg <57831615+tanujasheg@users.noreply.github.com>
2020-02-26 10:08:57 +0530

    initial

---
+++ src/newsapi.js
@@ -0,0 +1,9 @@
+export default{
+    search:function(searchTerm){
+        console.log('search....');
+        /*fetch(`https://newsapi.org/v2/top-headlines?country=in&apiKey=[171ab48f8a894889a3327230563b9593]`)
+        .then(res =>res.json())
+        .then(data =>console.log(data));*/
+       
+    }
+}
